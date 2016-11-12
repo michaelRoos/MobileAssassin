599c67079fe1b4d85e9e0b338ae1d884130e8a6c
Michael Roos <michael.d.roos@gmail.com>
2016-11-12 17:57:11 -0500

    sign in 2

--- app/src/main/java/net/mobileassassin/mobileassassin/GameList.java
+++ app/src/main/java/net/mobileassassin/mobileassassin/GameList.java
@@ -5,24 +5,26 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 import static android.R.attr.id;
 import static android.R.id.list;
 
 public class GameList extends AppCompatActivity {
 
+    String[] mobileArray = {"Game 1", "Game 2"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_list);
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_game_list, fetchNames());
-        ListView listView = (ListView) findViewById(list);
-        listView.setAdapter(adapter);
+        ArrayAdapter adapter  = new ArrayAdapter<String>(this, R.layout.activity_listview, mobileArray);
+        ListView listview = (ListView)findViewById(R.id.mobile_list);
+        listview.setAdapter(adapter);
     }
     private String[] fetchNames(){
         String[] a = {"Game 1", "Game 2"};
         return a;
     }
-
 
 
 
