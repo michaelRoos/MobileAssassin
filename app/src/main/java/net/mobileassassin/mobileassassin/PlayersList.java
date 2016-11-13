ccf69abb15129401d94be2d4ccbe0a1b36063958
Quinn Verbeke <ekebrev34@gmail.com>
2016-11-13 01:27:41 -0500

    Graphical Improvements

--- app/src/main/java/net/mobileassassin/mobileassassin/PlayersList.java
+++ app/src/main/java/net/mobileassassin/mobileassassin/PlayersList.java
@@ -2,6 +2,8 @@ package net.mobileassassin.mobileassassin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class PlayersList extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class PlayersList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_players_list);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_players_list,getArray());
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
+    }
+    private String[] getArray(){
+        //Incomplete
+        String[] asdf = {"Matthew \"The Noodle\" Mancini", "Mike Roos", "Ben Proto", "Dan Herlihy", "Quinn Verbeke"};
+        return asdf;
     }
 }
