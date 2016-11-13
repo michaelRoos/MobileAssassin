710e309aaffcb99c4b05cff2da64d5f62e86735e
Quinn Verbeke <ekebrev34@gmail.com>
2016-11-13 05:39:03 -0500

    killconfirm created, bugs worked out

---
+++ app/src/main/java/net/mobileassassin/mobileassassin/KillConfirm.java
@@ -0,0 +1,21 @@
+package net.mobileassassin.mobileassassin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class KillConfirm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kill_confirm);
+    }
+    public void confirm(View v){
+        //send a push notification to the killer that the kill was successful, and assign the
+        //other player a new target
+    }
+    public void deny(View v){
+        //send a push notification to the killer that the request to kill was denied
+    }
+}
