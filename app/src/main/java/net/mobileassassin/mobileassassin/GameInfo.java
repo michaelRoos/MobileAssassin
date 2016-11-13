3c727a78313dd976ec850d578d0608fe9f1b3ee9
Quinn Verbeke <ekebrev34@gmail.com>
2016-11-12 22:10:50 -0500

    GameInfo Basic layout and features implemented

--- app/src/main/java/net/mobileassassin/mobileassassin/GameInfo.java
+++ app/src/main/java/net/mobileassassin/mobileassassin/GameInfo.java
@@ -4,31 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
+
 
 public class GameInfo extends AppCompatActivity {
-
+    private String game;
+    public static String EXTRA_MESSAGE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_info);
 
         Intent intent = getIntent();
-        String message = intent.getStringExtra(GameList.EXTRA_MESSAGE);
-        Button viewPlayers = (Button)findViewById(R.id.view_players_button);
-        Button viewMap = (Button)findViewById(R.id.view_map_button);
-        viewPlayers.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        game = intent.getStringExtra(GameList.EXTRA_MESSAGE);
 
-            }
-        });
-        viewMap.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
 
-            }
-        });
+    }
+    public void playersClick(View v){
+        Intent intent = new Intent(this, PlayersList.class);
+        intent.putExtra(EXTRA_MESSAGE, game);
+        startActivity(intent);
+    }
+    public void mapClick(View v){
 
     }
 
