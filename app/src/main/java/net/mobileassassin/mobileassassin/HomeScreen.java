eaa637a3a5d346e108f816cfe69a514c653d9ad9
Quinn Verbeke <ekebrev34@gmail.com>
2016-11-13 00:23:01 -0500

    GameInfo Basic layout and features implemented

--- app/src/main/java/net/mobileassassin/mobileassassin/HomeScreen.java
+++ app/src/main/java/net/mobileassassin/mobileassassin/HomeScreen.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
